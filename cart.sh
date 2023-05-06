a9901798f05ee4eaa2591f19118709f2444cd062
Your Name <you@example.com>
2023-05-06 09:37:50 +0800

    Update README.md

---
+++ cart.sh
@@ -0,0 +1,15 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/cart.zip
+cd /app
+npm install
+
+cp cart.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
+
