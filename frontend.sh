6c73e1e1f30b46c86f71002dc48c63dad452c79f
Your Name <you@example.com>
2023-05-06 08:32:13 +0800

    Update README.md

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
-#yum install nginx -y
+yum install nginx -y
 
-#rm -rf /usr/share/nginx/html/*
-#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-#cd /usr/share/nginx/html
-#unzip /tmp/frontend.zip
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-#systemctl enable nginx
-#systemctl start nginx
+systemctl enable nginx
+systemctl start nginx
