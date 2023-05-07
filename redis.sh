bbc77e02134bafaff2acf3e8a3c46084beb7e4a7
Your Name <you@example.com>
2023-05-07 17:42:53 +0800

    Update README.md

--- redis.sh
+++ redis.sh
@@ -1,4 +1,5 @@
-script_path=$(dirname $0)
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
