699a8ecf36d595cb1a684d213d8f2404c7583e4b
Your Name <you@example.com>
2023-05-07 09:39:26 +0800

    Update README.md

--- redis.sh
+++ redis.sh
@@ -3,6 +3,7 @@ dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis/redis.conf /etc/redis.conf
-
+echo -e "\e[32m********* Replaced  Started Successfully ****************\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
+echo -e "\e[32m********* Redis Started Successfully ****************\e[0m"
