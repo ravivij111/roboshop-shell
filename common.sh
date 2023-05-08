ee5fef189dcf8f99062476f5c01d76992983064a
Your Name <you@example.com>
2023-05-08 14:56:55 +0800

    Update README.md

--- common.sh
+++ common.sh
@@ -1,9 +1,21 @@
 app_user=roboshop
+script=$(realpath "$0")
+scipt_path=$(dirname "$script")
+log_file=/tmp/roboshop.log
+func_stat_check(){
+  if [ $1 -eq 0 ] ; then
+    echo -e "\e[32mSUCESS\e[0m"
+  else
+    echo -e "\e[31mFailure\e[0m"
+    echo "refer the log file /tmp/roboshop.log"
+    exit 1
+  fi
+}
 
 func_java(){
 
-  yum install maven -y
-
+  yum install maven -y &>> $log_file
+  func_stat_check $?
   func_app_prereq
   mvn clean package
   mv target/${component}-1.0.jar ${component}.jar
@@ -28,6 +40,7 @@ func_schema_setup(){
  if [ "$schema_setup" ==  "mongo" ]; then
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
     yum install mongodb-org-shell -y
+    func_stat_check
     mongo --host mongodb-dev.r1devopsb.online </app/schema/${component}.js
     printhead "Connected to MongoDB Successfully"
   elif [ "$schema_setup" ==  "mysql" ]; then
@@ -40,12 +53,13 @@ func_schema_setup(){
 
 func_nodejs(){
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  yum install nodejs -y
+  yum install nodejs -y &>> $log_file
   func_app_prereq
   cd /app
   unzip /tmp/${component}.zip
   cd /app
-  npm install
+  npm install &>> $log_file
+  func_stat_check
   func_print_head "Installed Dependencies"
   func_systemd_setup
   func_schema_setup
@@ -55,6 +69,6 @@ func_systemd_setup(){
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
   systemctl daemon-reload
   systemctl enable cart
-  systemctl restart ${component}
+  systemctl restart ${component} &>> $log_file
   func_print_head ${component}" service started"
 }
