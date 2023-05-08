c7bec98aa99cf05789c09dbae87675fcf64d0487
Your Name <you@example.com>
2023-05-08 14:06:57 +0800

    Update README.md

--- common.sh
+++ common.sh
@@ -3,34 +3,23 @@ app_user=roboshop
 func_java(){
 
   yum install maven -y
+
+  func_app_prereq
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+  printhead " ${component} service enabled erlang "
+  func_systemd_setup
+}
+
+func_app_prereq(){
   useradd ${app_user}
+  rm -rf /app
   mkdir /app
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
   unzip /tmp/${component}.zip
 
-  mvn clean package
-  mv target/${component}-1.0.jar ${component}.jar
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-
-
-  printhead " ${component} service enabled erlang "
-  yum install mysql -y
-  mysql -h mysql-dev.r1devopsb.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
-  #RoboShop@1
-
-  printhead " Installed MySQL in ${component}  "
-
-
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
-
-  printhead " Restarted the ${component} services "
-
 }
-
-
 
 func_print_head(){
    echo -e "\e[32m********* $* ****************\e[0m"
@@ -41,28 +30,31 @@ func_schema_setup(){
     yum install mongodb-org-shell -y
     mongo --host mongodb-dev.r1devopsb.online </app/schema/${component}.js
     printhead "Connected to MongoDB Successfully"
+  elif [ "$schema_setup" ==  "mysql" ]; then
+    yum install mysql -y
+    mysql -h mysql-dev.r1devopsb.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+    #RoboShop@1
+    printhead " Installed MySQL in ${component}  "
   fi
 }
 
 func_nodejs(){
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd $app_user
-rm -rf /app
-mkdir /app
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-cd /app
-unzip /tmp/${component}.zip
-cd /app
-npm install
-func_print_head "Installed Dependencies"
-cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart ${component}
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  yum install nodejs -y
+  func_app_prereq
+  cd /app
+  unzip /tmp/${component}.zip
+  cd /app
+  npm install
+  func_print_head "Installed Dependencies"
+  func_systemd_setup
+  func_schema_setup
+}
 
-func_print_head ${component}" service started"
-
-func_schema_setup
-
+func_systemd_setup(){
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  systemctl daemon-reload
+  systemctl enable cart
+  systemctl restart ${component}
+  func_print_head ${component}" service started"
 }
--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,8 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
-
+component="shipping"
+schema_setup=mysql
 if [ -z "$mysql_root_password" ]
 then
   echo Input mySQL password is missing
