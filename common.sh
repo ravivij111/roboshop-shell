5840d6a63f3891e487039a6f0a8d81ac9d7759e2
Your Name <you@example.com>
2023-05-08 13:45:35 +0800

    Update README.md

--- common.sh
+++ common.sh
@@ -1,4 +1,37 @@
 app_user=roboshop
+
+func_java(){
+
+  yum install maven -y
+  useradd ${app_user}
+  mkdir /app
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+  unzip /tmp/${component}.zip
+
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+
+  printhead " ${component} service enabled erlang "
+  yum install mysql -y
+  mysql -h mysql-dev.r1devopsb.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+  #RoboShop@1
+
+  printhead " Installed MySQL in ${component}  "
+
+
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl restart ${component}
+
+  printhead " Restarted the ${component} services "
+
+}
+
+
+
 print_head(){
    echo -e "\e[32m********* $* ****************\e[0m"
 }
@@ -6,7 +39,7 @@ schema_setup(){
  if [ "$schema_setup" ==  "mongo" ]; then
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
     yum install mongodb-org-shell -y
-    mongo --host mongodb-dev.r1devopsb.online </app/schema/catalogue.js
+    mongo --host mongodb-dev.r1devopsb.online </app/schema/${component}.js
     printhead "Connected to MongoDB Successfully"
   fi
 }
--- shipping.sh
+++ shipping.sh
@@ -9,28 +9,4 @@ then
   exit
 fi
 
-yum install maven -y
-useradd ${app_user}
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
-
-printhead " shipping service enabled erlang "
-yum install mysql -y
-mysql -h mysql-dev.r1devopsb.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-#RoboShop@1
-
-printhead " Installed MySQL in Shipping  "
-
-
-systemctl restart shipping
-
-printhead " Restarted the services "
+func_java
