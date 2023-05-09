05ef3099b8c13c00fea35fd458e0e20f4b474f25
Your Name <you@example.com>
2023-05-10 07:33:10 +0800

    Update README.md

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -23,10 +23,20 @@ func_stat_check $?
 #func_print_head "  RabbitMQ Service Status check  "
 #systemctl status rabbitmq-server
 #func_print_head " Completed RabbitMQ Service Status check  "
-func_print_head " Add user "
-rabbitmqctl add_user roboshop ${rabbit_mq_password} &>> /tmp/roboshop.log
-#roboshop123
-func_stat_check $?
+
+id roboshop &>> /tmp/roboshop.log
+  #id gives 0 if user is there else it gives 1
+  if [ $? -ne 0 ]; then
+    func_print_head " Add user "
+    rabbitmqctl add_user roboshop ${rabbit_mq_password} &>> /tmp/roboshop.log
+    #roboshop123
+    func_stat_check $?
+  else
+    func_print_head "roboshop user already exists "
+  fi
+
+
+
 
 func_print_head " Set the permissions to user  "
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> /tmp/roboshop.log
