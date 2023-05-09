3edb8c34e2acee9e6d9506b2bd8086d8ea2d3e11
Your Name <you@example.com>
2023-05-10 07:16:20 +0800

    Update README.md

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,21 +8,28 @@ then
   echo Imput Roboshop Appuser password is missing
   exit
 fi
-
+func_print_head " installing erlang  "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 yum install erlang -y
-func_print_head " installed erlang  "
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> /tmp/roboshop.log
-func_stat_check $?
+func_print_head $?
+
 func_print_head " Install RabbitMQ "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> /tmp/roboshop.log
 yum install rabbitmq-server -y &>> /tmp/roboshop.log
 func_stat_check $?
+func_print_head " Starting RabbitMQ service "
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server &>> /tmp/roboshop.log
 func_stat_check $?
-func_print_head " RabbitMQ Started and confirm by systemctl status rabbitmq-server "
+
+systemctl status rabbitmq-server
+func_print_head " Completed RabbitMQ Service Status check  "
+func_print_head " Add user "
 rabbitmqctl add_user roboshop ${rabbit_mq_password}
 #roboshop123
+func_stat_check $?
+
+func_print_head " Set the permissions to user  "
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> /tmp/roboshop.log
 func_stat_check $?
 func_print_head " Completed MQ SetUp  "
